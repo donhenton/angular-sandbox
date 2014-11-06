bdd2ba5006de95cd208f3e922eb43ab323a401b7
d <dhenton@localhost.com>
2014-11-06 07:49:13 -0600

    doc

--- public_html/js/app/services/messageFactory.js
+++ public_html/js/app/services/messageFactory.js
@@ -1,22 +1,4 @@
-/**
- * 
- * http://answers.oreilly.com/topic/2190-two-examples-of-the-observer-pattern-in-javascript/
- * 
- * This is simple message pump system. Clients register a function for a particular 
- * event by calling:
- * 
- * subscribe(functionVar,eventType)
- * subscribe(refreshListener,"ON_REFRESH");
- * 
- * To raise an event
- * raiseEvent(messagePayload,eventType)
- * raiseEvent({username:'fred'},"ON_REFRESH")
- * 
- * the signature of the refreshListener should be : refreshListener(payload)
- * The payload is what will be sent for the event.
- * 
- * 
- */
+
 
 (function () {
     var messageFactory = function ($log)
