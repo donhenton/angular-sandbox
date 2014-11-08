d8ce017f7a5b3233408d4b0aafebbcfd517f5ff7
Don <Don@alphamail>
2014-11-07 19:55:05 -0600

    formatting

--- public_html/js/app/controllers/reviewController.js
+++ public_html/js/app/controllers/reviewController.js
@@ -12,14 +12,32 @@
             $scope.currentReviews =  reviewFactory.scatterCurrentReviews();
    
         }
+        
+        var resetReviews = function()
+        {
+            $scope.currentReviews.forEach(function(r)
+            {
+                r.isEditing = false;
+            }
+                    )
+        }
 
         messageFactory.subscribe(newRestaurantHandler,"ON_RESTAURANT_CHANGE");
         
         $scope.editReview = function(review)
         {
+            resetReviews();
             review.isEditing=true;
         }
-        
+        $scope.saveReviewEdit = function(review)
+        {
+            review.isEditing=true;
+        }
+        $scope.cancelReviewEdit = function(review)
+        {
+           // review.isEditing=true;
+            resetReviews();
+        }
     }
     reviewController.$inject = ['$scope', '$log', 'reviewFactory', 'messageFactory','restaurantFactory'];
     angular.module('restaurantApp')
