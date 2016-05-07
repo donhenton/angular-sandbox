6965b5139a4abf3c65660d1e5ff00e00fb28ee43
Don Henton <donhenton@gmail.com>
2016-05-07 07:18:11 -0500

    adding dismissible alert 2

--- public_html/waitAssets/js/controllers/sectionController.js
+++ public_html/waitAssets/js/controllers/sectionController.js
@@ -8,17 +8,37 @@ angular.module('waitApp')
         .controller('SectionCtrl', function ($scope, $log, $timeout)
         {
             var vm = this;
+             
+
+            vm.displayBanner = function()
+            {
+                vm.showBanner = true;
+
+                    $timeout(function () {
+                        vm.removeBanner();
+
+                    }, 10000);
+            }
+            
+            
+            vm.removeBanner = function ()
+            {
+                    vm.showBanner = false;
+            }
 
             vm.successAction = function ()
             {
                 $log.debug("did success")
                 vm.loading = true;
                 vm.error = false;
-                vm.data = [1,2,3,4];
+                vm.data = [1, 2, 3, 4];
+                vm.displayBanner();
 
+                
                 $timeout(function () {
                     vm.loading = false;
                     vm.error = false;
+
                     $log.debug("did change");
                     angular.forEach(vm.data, function (data, idx)
                     {
@@ -28,15 +48,15 @@ angular.module('waitApp')
 
                 }, 2000)
             }
-            
+
             vm.failureAction = function ()
             {
                 $log.debug("did failure")
                 vm.loading = true;
                 vm.error = false;
-                vm.data = [1,2,3,4];
-                vm.errorMessage="Server Error: Try again later"
-
+                vm.data = [1, 2, 3, 4];
+                vm.errorMessage = "Server Error: Try again later"
+                vm.displayBanner();
                 $timeout(function () {
                     vm.loading = false;
                     vm.error = true;
@@ -49,7 +69,7 @@ angular.module('waitApp')
 
                 }, 2000)
             }
-            
+
             vm.successAction();
         });
 
